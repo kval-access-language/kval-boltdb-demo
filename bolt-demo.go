11378f343283d8890310bcc30298d7f58f6e8e69
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-22 02:07:05 +1300

    [refactor] Gofmt.

--- bolt-demo.go
+++ bolt-demo.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-   "os"
-   "fmt"
-   kval "github.com/kval-access-language/kval-boltdb"
-) 
+	"fmt"
+	kval "github.com/kval-access-language/kval-boltdb"
+	"os"
+)
 
 func main() {
 
-   kb, err := kval.Connect("newdb.bolt")
-   if err != nil {
-      fmt.Fprintf(os.Stderr, "Error opening bolt database: %#v", err)
-      os.Exit(1)
-   }
-   defer kval.Disconnect(kb)
+	kb, err := kval.Connect("newdb.bolt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening bolt database: %#v", err)
+		os.Exit(1)
+	}
+	defer kval.Disconnect(kb)
 
 	//Lets do a test insert...
 	res, _ := kval.Query(kb, "INS test bucket one >> test bucket two >>>> key one :: value one")
@@ -33,21 +33,21 @@ func main() {
 	//we should have two results, so may need a loop to access...
 	fmt.Printf("How many results in GET: %d\n", len(get.Result))
 
-	for k, v := range(get.Result) {
+	for k, v := range get.Result {
 		fmt.Printf("Key: %s, Value: %s\n", k, v)
 	}
 
 	//binary objects?
 	_ = kval.StoreBlob(kb, "INS binary data >>>> data one", "text/plain", []byte("some data"))
-	
+
 	//get it back?
 	data, _ := kval.Query(kb, "GET binary data >>>> data one")
 
-	//UnwrapBlob and GetblobData are helpers to access the data, but let's just print it	
+	//UnwrapBlob and GetblobData are helpers to access the data, but let's just print it
 	fmt.Printf("Encoded data: %s\n", data.Result["data one"])
 
 	//How about renaming our data?
-	kval.Query(kb, "REN test bucket one >> test bucket two => new name for bucket two")	
+	kval.Query(kb, "REN test bucket one >> test bucket two => new name for bucket two")
 
 	//repeat how we retrieved the data last time...
 	get, _ = kval.Query(kb, "GET test bucket one >> new name for bucket two")
@@ -55,7 +55,7 @@ func main() {
 	//we should have two results, so may need a loop to access...
 	fmt.Printf("How many results in GET: %d\n", len(get.Result))
 
-	for k, v := range(get.Result) {
+	for k, v := range get.Result {
 		fmt.Printf("Key: %s, Value: %s\n", k, v)
 	}
 
@@ -63,13 +63,13 @@ func main() {
 	lis, _ = kval.Query(kb, "LIS test bucket one >> test bucket two")
 
 	fmt.Printf("Bucket still exists? %v\n", lis.Exists)
-	
+
 	//delete is easy too...
-	del, _ := kval.Query(kb, "DEL test bucket one")	
+	del, _ := kval.Query(kb, "DEL test bucket one")
 
 	//any data left?
 	fmt.Printf("Bucket keys:  %d\n", del.Stats.KeyN)
-	fmt.Printf("Bucket depth: %d\n", del.Stats.Depth)	
+	fmt.Printf("Bucket depth: %d\n", del.Stats.Depth)
 
 	if del.Stats.KeyN == 0 && del.Stats.Depth == 0 {
 		fmt.Println("Thanks for checking out this demo Gophers!")
